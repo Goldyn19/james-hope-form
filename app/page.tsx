ed7f80181d0950362a86a46bd68ef715ffe5cb8f
Goldyn19 <nwaezegolden2002@gmail.com>
2025-05-05 15:10:22 +0100

    changed education to drop down

--- app/page.tsx
+++ app/page.tsx
@@ -5,31 +5,29 @@ import Image from "next/image";
 export default function Home() {
   const router = useRouter();
 
-
   const [fullname, setFullname] = useState("");
   const [email, setEmail] = useState("");
-  
+
   const [phoneNumber, setPhoneNumber] = useState("");
   const [location, setLocation] = useState("");
   const [levelOfEducation, setLevelOfEducation] = useState("");
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
 
   const [currentTime, setCurrentTime] = useState(new Date());
 
   const [loading, setLoading] = useState(false);
 
-  
-
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError("");
     setSuccess(false);
     setLoading(true);
-  
+
     // Replace with your actual Google Form URL and entry IDs
-    const googleFormURL = "https://docs.google.com/forms/d/e/1FAIpQLSej5zLuE0py92LAQnGEOW6ygd0mRmYwrI7YoNRgK7mU_qNbmg/formResponse";
-  
+    const googleFormURL =
+      "https://docs.google.com/forms/d/e/1FAIpQLSej5zLuE0py92LAQnGEOW6ygd0mRmYwrI7YoNRgK7mU_qNbmg/formResponse";
+
     // Map React form fields to Google Form entry IDs
     const formData = new FormData();
     formData.append("entry.1591408324", fullname); // Replace with your entry ID
@@ -37,7 +35,7 @@ export default function Home() {
     formData.append("entry.1790398587", phoneNumber); // Replace with your entry ID
     formData.append("entry.1609829514", location); // Replace with your entry ID
     formData.append("entry.306990567", levelOfEducation); // Replace with your entry ID
-  
+
     fetch(googleFormURL, {
       method: "POST",
       mode: "no-cors", // Required for Google Forms submission
@@ -47,16 +45,17 @@ export default function Home() {
         setSuccess(true);
         setLoading(false);
         setTimeout(() => {
-          router.push("https://portal.jhu.edu.ng/Applicant/ApplicantAccount/SignUp"); // Redirect or show a success message
+          router.push(
+            "https://portal.jhu.edu.ng/Applicant/ApplicantAccount/SignUp"
+          ); // Redirect or show a success message
         }, 3000);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error submitting form:", error);
         setError("Failed to submit. Please try again.");
         setLoading(false);
       });
   };
-  
 
   useEffect(() => {
     const timerId = setInterval(() => {
@@ -115,10 +114,7 @@ export default function Home() {
   const formattedTime = currentTime.toLocaleTimeString();
   const formattedDateInWords = formatDateToWords(currentTime);
   return (
-    <div
-      className="w-full min-h-dvh  py-5 px-6 md:px-0 text-black bg-amber-50"
-      
-    >
+    <div className="w-full min-h-dvh  py-5 px-6 md:px-0 text-black bg-amber-50">
       <h2 className="w-full text-center text-lg md:text-2xl flex flex-col md:flex-row justify-center items-center gap-4">
         <Image
           src="/The-Logo.png"
@@ -127,7 +123,7 @@ export default function Home() {
           height={40}
           width={40}
         />
-        James Hope University 
+        James Hope University
       </h2>
       <h4 className="w-full text-center text-lg md:text-2xl my-4 font-bold">
         Fill The form
@@ -136,94 +132,113 @@ export default function Home() {
         <h6>
           {formattedDateInWords} {formattedTime}
         </h6>
-        {success? ( <div className="w-full">
-              <img
-                src="/successful.gif"
-                alt="Success"
-                className="mx-auto"
+        {success ? (
+          <div className="w-full">
+            <img src="/successful.gif" alt="Success" className="mx-auto" />
+            <h1 className="text-center text-custom-blue text-heading-s mt-5">
+              Registration Successful!
+            </h1>
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit} className="grid gap-4 py-4 w-full">
+            <div className="space-y-2">
+              <label
+                htmlFor="name"
+                className="text-sm font-medium leading-none"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
+                placeholder="Enter Full Name"
+                onChange={(e) => setFullname(e.target.value)}
               />
-              <h1 className="text-center text-custom-blue text-heading-s mt-5">
-                Registration Successful!
-              </h1>
-            </div>):(
-              <form onSubmit={handleSubmit} className="grid gap-4 py-4 w-full">
-              <div className="space-y-2">
-                <label htmlFor="name" className="text-sm font-medium leading-none">
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
-                  placeholder="Enter Full Name"
-                  onChange={(e) => setFullname(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <label htmlFor="Email" className="text-sm font-medium leading-none">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <label
-                  htmlFor="phone number"
-                  className="text-sm font-medium leading-none"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="text"
-                  className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
-                  placeholder="Phone Number"
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <label
-                  htmlFor="password"
-                  className="text-sm font-medium leading-none"
-                >
-                  Location
-                </label>
-                <input
-                  type="text"
-                  className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
-                  placeholder="Enter Your Location"
-                  value={location}
-                  onChange={(e) => setLocation(e.target.value)}
-                />
-              </div>
-              <div className="space-y-2">
-                <label
-                  htmlFor="country"
-                  className="text-sm font-medium leading-none"
-                >
-                  Level of Education
-                </label>
-                <input
-                  type="text"
-                  className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
-                  placeholder="Level of Education"
-                  onChange={(e) => setLevelOfEducation(e.target.value)}
-                />
-              </div>
-              {error && <p className="text-red-500 items-center mt-5 text-body-s">{error}</p>}
-              <button className="inline-flex items-center justify-center rounded-md text-sm font-medium bg-[#66001a] h-10 px-4 py-2 text-white hover:bg-slate-700">
-              {loading ? "Creating..." : "Submit"}
-              </button>
-            </form>
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="Email"
+                className="text-sm font-medium leading-none"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="phone number"
+                className="text-sm font-medium leading-none"
+              >
+                Phone Number
+              </label>
+              <input
+                type="text"
+                className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
+                placeholder="Phone Number"
+                onChange={(e) => setPhoneNumber(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="password"
+                className="text-sm font-medium leading-none"
+              >
+                Location
+              </label>
+              <input
+                type="text"
+                className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
+                placeholder="Enter Your Location"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <label
+                htmlFor="education"
+                className="text-sm font-medium leading-none"
+              >
+                Level of Education
+              </label>
+              <select
+                id="education"
+                className="flex h-10 w-full rounded-md border border-slate-300 px-3 py-2 text-sm text-black focus:outline-none focus:ring-0"
+                onChange={(e) => setLevelOfEducation(e.target.value)}
+                defaultValue=""
+              >
+                <option value="" disabled>
+                  Select level
+                </option>
+                <option value="O Level">O Level</option>
+                <option value="HND">HND</option>
+                <option value="B.Sc.">B.Sc.</option>
+                <option value="M.Sc.">M.Sc.</option>
+                <option value="PhD">PhD</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+
+            {error && (
+              <p className="text-red-500 items-center mt-5 text-body-s">
+                {error}
+              </p>
             )}
+            <button className="inline-flex items-center justify-center rounded-md text-sm font-medium bg-[#66001a] h-10 px-4 py-2 text-white hover:bg-slate-700">
+              {loading ? "Creating..." : "Submit"}
+            </button>
+          </form>
+        )}
 
         <p className="text-xs text-black w-full text-center">
           © Copyright 2025 New Horizons Nigeria, All Rights Reserved.
         </p>
       </div>
     </div>
-  )
+  );
 }
